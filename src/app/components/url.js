2f80dfe2208ee2d1ea537b1f16d4f8ae382bba27
Destiny653 <fokundem.com@gmail.com>
2025-02-15 07:50:21 +0100

    ff

--- src/app/components/url.js
+++ src/app/components/url.js
@@ -1,2 +1,2 @@
-export const basicUrl ='http://localhost:3000'
-// export const basicUrl ='https://geni-backend.onrender.com'
+// export const basicUrl ='http://localhost:3000'
+export const basicUrl ='https://geni-backend.onrender.com'
